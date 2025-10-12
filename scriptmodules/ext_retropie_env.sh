b187d6016f54e11526682b867f5c000daf00e4b7
LeonardWard <pang.e1311@outlook.com>
2025-10-12 10:34:33 +0900

    변수 경로 업데이트

--- scriptmodules/ext_retropie_env.sh
+++ scriptmodules/ext_retropie_env.sh
@@ -6,6 +6,6 @@
 # RetroPie-Setup 스크립트와의 호환성을 위해 필요한 환경 변수들을 여기에 정의합니다.
 # ===============================================
 
-export __swapdir="/tmp/swap"
-export biosdir="/home/pangui/share/bios"
-export md_conf_root="/home/pangui/share/system/configs"
+export __swapdir="$INSTALL_BUILD_DIR/swap"
+export biosdir="$USER_BIOS_PATH"
+export md_conf_root="$USER_SYSTEM_PATH"
