5d60ecf40d044b6d02d3f695771eeeeda5028f79
LeonardWard <pang.e1311@outlook.com>
2025-10-23 19:05:41 +0900

    업데이트

--- debug_cores.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# 파일명: debug_cores.sh
-
-source "/home/pangui/scripts/retropangui/retropangui_setup.sh"
-
-source "/home/pangui/scripts/retropangui/scriptmodules/install_base_4_in_5_cores.sh"
-
-install_base_cores || exit 1
