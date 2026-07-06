fa7cee367242952b7f4bdf8d4965aa2dfe99d985
LeonardWard <ayabrea04@gmail.com>
2026-07-07 00:45:14 +0900

    uim 빌드 에러 수정: installed-modules.scm 생성 단계 크로스 실행 문제
    scm/installed-modules.scm을 만드는 단계가 방금 크로스 컴파일한
    uim-module-manager(aarch64)를 빌드 호스트(x86_64)에서 직접 실행하려고
    해서 "cannot execute binary file: Exec format error"로 실패함 - 이
    단계는 Scheme 인터프리터로 각 입력기 모듈을 실제로 로드해봐야 결과를
    만들 수 있는 구조라 정적으로 미리 만들어둘 수 없음.
    
    Buildroot의 guile 패키지가 guild 컴파일 단계에서 겪는 것과 동일한
    종류의 문제 - guile.mk의 HOST_GUILE + GUILE_FOR_BUILD 패턴을 그대로
    따라서 host-uim(네이티브 x86_64용)을 추가로 빌드하고, UIM_MAKE_OPTS로
    타깃 빌드가 이 host-uim의 uim-module-manager를 대신 쓰도록 함.
    LIBUIM_PLUGIN_LIB_DIR도 타깃(aarch64) .so 대신 host-uim 자신의 plugin
    디렉토리를 가리키게 함(host 바이너리가 dlopen할 땐 host-arch 라이브러리여야
    함).
    
    우선 가장 유력한 원인(크로스 실행 실패)에 대한 표준 Buildroot 패턴으로
    수정 - "look" 모듈처럼 네이티브 플러그인(.so)에 의존하는 항목이 있다면
    추가 문제가 나올 수 있어 재빌드 결과 확인 필요.

--- br2-external/package/uim/uim.mk
+++ br2-external/package/uim/uim.mk
@@ -8,7 +8,20 @@ UIM_VERSION = 1.9.6
 UIM_SITE = https://github.com/uim/uim/releases/download/$(UIM_VERSION)
 UIM_LICENSE = BSD-3-Clause
 UIM_LICENSE_FILES = COPYING
-UIM_DEPENDENCIES = ncurses gettext host-pkgconf
+UIM_DEPENDENCIES = ncurses gettext host-pkgconf host-uim
+
+# 2026-07-07: uim 빌드 중 scm/installed-modules.scm을 생성하는 단계가 방금 막
+# 빌드한 uim-module-manager 실행파일을 직접 실행해서 결과를 만들어냄(Scheme
+# 인터프리터로 각 입력기 모듈을 실제로 로드해봐야 하는 구조라 정적으로 미리
+# 만들어둘 수 없음) - 크로스 컴파일이라 그 실행파일이 aarch64용이라 빌드
+# 호스트(x86_64)에서 실행이 안 돼 "Exec format error"로 실패함(Buildroot의
+# guile 패키지가 guild 컴파일 단계에서 겪는 것과 동일한 문제 - guile.mk의
+# HOST_GUILE + GUILE_FOR_BUILD 패턴을 그대로 따름). host-uim으로 네이티브
+# x86_64용 uim-module-manager를 별도로 빌드해서 그걸 대신 쓰게 함.
+UIM_BUILDDIR = $(BUILD_DIR)/uim-$(UIM_VERSION)
+UIM_MAKE_OPTS = \
+	UIM_MODULE_MANAGER=$(HOST_DIR)/bin/uim-module-manager \
+	UIM_MODULE_MANAGER_ENV="LIBUIM_SYSTEM_SCM_FILES=$(UIM_BUILDDIR)/sigscheme/lib LIBUIM_SCM_FILES=$(UIM_BUILDDIR)/scm LIBUIM_PLUGIN_LIB_DIR=$(HOST_DIR)/lib/uim/plugin UIM_DISABLE_NOTIFY=1"
 
 # 2026-07-07: uim이 내장한 sigscheme의 libgcroots가 공유 라이브러리(.so)로
 # 빌드되는데, 이건 시스템에 install되는 게 아니라 uim 빌드 트리 안에만 있는
@@ -58,4 +71,8 @@ UIM_CONF_OPTS = \
 	--disable-notify \
 	--enable-fep
 
+HOST_UIM_DEPENDENCIES = host-ncurses host-gettext host-pkgconf
+HOST_UIM_CONF_OPTS = $(UIM_CONF_OPTS)
+
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
