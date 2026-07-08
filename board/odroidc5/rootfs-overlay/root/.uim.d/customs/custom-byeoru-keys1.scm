4e8623df0229ff251d1482639d14cd4e61369b61
LeonardWard <ayabrea04@gmail.com>
2026-07-08 22:41:29 +0900

    uim: byeoru 한/영 전환키에 Alt_R/Hangul 중복 매핑 추가
    Ctrl+Space 하나만으로는 불편할 수 있어서, 오른쪽 Alt(Alt_R)와 실제
    한/영 키(Hangul, 지원하는 키보드 한정)도 같은 전환 동작을 하도록 같은
    리스트에 추가. uim은 한 액션에 여러 키를 동시에 묶을 수 있음(기본값도
    원래 zenkaku-hankaku+Shift+Space 두 개였음).

--- board/odroidc5/rootfs-overlay/root/.uim.d/customs/custom-byeoru-keys1.scm
+++ board/odroidc5/rootfs-overlay/root/.uim.d/customs/custom-byeoru-keys1.scm
@@ -5,5 +5,10 @@
 ; uim 사용자들 사이에 콘솔 환경에서 흔히 보고되는 문제) 전환이 안 됨.
 ; Ctrl+Space는 NUL(0x00) 제어 바이트로 확실히 구분되는 바이트를 만들어내서
 ; 콘솔 환경에서도 안정적으로 동작함을 실기기에서 확인 - 이 키로 교체.
-(custom-set-value! (quote byeoru-on-key) (list "<Control> "))
-(custom-set-value! (quote byeoru-latin-key) (list "<Control> "))
+; 오른쪽 Alt(Alt_R)와 실제 한/영 키(Hangul, 있는 키보드 한정)도 같은 동작을
+; 하도록 중복 매핑 - uim은 한 액션에 여러 키를 리스트로 묶을 수 있음(기본값도
+; 원래 "zenkaku-hankaku"와 "<Shift> " 두 개가 같이 묶여 있었음). 모디파이어
+; 없는 단일 키는 꺾쇠 없이 키심 이름 그대로 씀(uim이 X11/XKB 키심 이름 규약을
+; 그대로 씀 - kmscon도 libxkbcommon 기반이라 같은 규약).
+(custom-set-value! (quote byeoru-on-key) (list "<Control> " "Alt_R" "Hangul"))
+(custom-set-value! (quote byeoru-latin-key) (list "<Control> " "Alt_R" "Hangul"))
